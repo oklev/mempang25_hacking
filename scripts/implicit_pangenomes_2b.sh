9c58dcdbc5a569fc1e9aa8bcd64397ce2fe81ddd
Oliver Levine <oliverklevine@uga.edu>
2025-05-14 14:45:44 -0400

    add faidx step & some condititonals

--- scripts/implicit_pangenomes_2b.sh
+++ scripts/implicit_pangenomes_2b.sh
@@ -6,9 +6,14 @@ cd day2_impg/assemblies
 if [ ! -f primates16.hsa6.fa.gz ]; then #only download the file if it isn't already there
     wget https://garrisonlab.s3.amazonaws.com/teaching/primates16.hsa6.fa.gz
 fi
+if [ !-f primates16.hsa6.fa.gz.fai ]; then
+    samtools faidx primates16.hsa6.fa.gz
+fi
 
 cd $DIR_MEMPANG25/day2_impg
 mkdir -p alignments
 
 # IT TAKES TIME
-wfmash assemblies/primates16.hsa6.fa.gz --target-prefix chm13 -t 32 -p 95 > alignments/primates16-vs-chm13.hsa6.paf
+if [ ! -f alignments/primates16-vs-chm13.hsa6.paf ]; then
+    wfmash assemblies/primates16.hsa6.fa.gz --target-prefix chm13 -t 32 -p 95 > alignments/primates16-vs-chm13.hsa6.paf
+fi
